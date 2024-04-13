c84a1873030e6cff482f5e311485dd48b77eaa2d
Vidya Prakash Peram <vidyaprakashperam@Vidyas-MacBook-Air.local>
2024-04-12 21:59:23 -0400

    organizing files

--- sqlalchemy/sqlalchemy_script.py
+++
@@ -1,8 +0,0 @@
-import sqlalchemy
-from sqlalchemy import *
-
-engine = create_engine("sqlite+pysqlite:///:memory:")
-connect = engine.connect()
-results = connect.execute(text("SELECT 'Hello World"))
-
-
